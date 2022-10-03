a5e32de84f3c2db6ee0150d09a24a0cc1e887153
Cucius <Rylan19@Gmail.com>
2022-10-02 19:23:44 -0600

    Added the code for  models

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: formatDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const User = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+const invalidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [invalidEmail, "Not a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
