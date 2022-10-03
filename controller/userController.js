2243dbd128241fead30562d8e9d7a444c03cb7dc
Cucius <Rylan19@Gmail.com>
2022-10-02 22:43:15 -0600

    Added update methods for User and update method to add friends

--- controller/userController.js
+++ controller/userController.js
@@ -18,8 +18,26 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  updateUser(req, res) {},
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $set: req.body }, { runValidators: true, new: true })
+      .select("-__v")
+      .populate(["thoughts", "friends"])
+      .then((user) => (!user ? res.status(404).json({ message: "No user with this id!" }) : res.json(user)))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   deleteUser(req, res) {},
-  addFriend(req, res) {},
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { runValidators: true, new: true })
+      .select("-__v")
+      .populate(["thoughts", "friends"])
+      .then((user) => (!user ? res.status(404).json({ message: "No user with this id!" }) : res.json(user)))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   deleteFriend(req, res) {},
 };
