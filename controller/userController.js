20a4e7a4b6117ee6559e07d7d2447ed0d28fcab9
Cucius <Rylan19@Gmail.com>
2022-10-02 22:20:34 -0600

    Added methods to get Users single or all & create user option

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,23 @@
-const { User } = require("../models");
+const User = require("../models/User");
 
 module.exports = {
-  getUsers(req, res) {},
-  getSingleUser(req, res) {},
-  createUser(req, res) {},
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .populate(["thoughts", "friends"])
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
   updateUser(req, res) {},
   deleteUser(req, res) {},
   addFriend(req, res) {},
